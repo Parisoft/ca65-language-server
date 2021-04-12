7599e3f1992b135733e1f2cad1695374aeca091c
Andre Paris <andreparis@gmail.com>
2021-04-12 09:18:25 -0300

    1st commit

---
+++ ca65-lsp/src/main/java/com/parisoft/ca65/lsp/server/CA65TextDocumentService.java
@@ -0,0 +1,32 @@
+package com.parisoft.ca65.lsp.server;
+
+import org.eclipse.lsp4j.*;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.eclipse.lsp4j.services.TextDocumentService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class CA65TextDocumentService implements TextDocumentService {
+
+    @Override
+    public void didOpen(DidOpenTextDocumentParams didOpenTextDocumentParams) {
+
+    }
+
+    @Override
+    public void didChange(DidChangeTextDocumentParams didChangeTextDocumentParams) {
+
+    }
+
+    @Override
+    public void didClose(DidCloseTextDocumentParams didCloseTextDocumentParams) {
+
+    }
+
+    @Override
+    public void didSave(DidSaveTextDocumentParams didSaveTextDocumentParams) {
+
+    }
+}
